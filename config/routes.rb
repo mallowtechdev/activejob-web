0426614fb8b2be515f136d375e3d0f2b95b7d010
Gowtham Kuppusamy <gowtham.kuppusamy@mallow-tech.com>
2023-11-18 10:46:11 +0530

    Issue-6: Remove the unwanted customisation route comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,5 @@ Rails.application.routes.draw do
   root 'activejob_web/jobs#index'
   namespace :activejob_web do
     resources :jobs, only: %i[index show edit update]
-    # if Rails.application.config.enable_custom_routes
-    #   # Define a custom lambda function for route matching
-    #   custom_lambda = lambda do |_request|
-    #     job = ActivejobWeb::Job.first
-    #     job.id == '95ee9435-2b03-42aa-8132-2716a019e51f'
-    #   end
-    #   # get 'jobs/:id/edit', to: 'jobs#edit', constraints: UserRoleConstraint.new(custom_lambda)
-    #   get 'jobs/:id/edit', to: 'jobs#edit', constraints: custom_lambda
-    # else
-    #   get 'jobs/:id/edit', to: 'jobs#edit'
-    # end
   end
 end
