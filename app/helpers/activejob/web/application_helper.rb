14d52c60f4138d065a2679591fbb3781908164a7
Surender <surender@mallow-tech.com>
2024-04-23 19:01:44 +0530

    Removed signed_in_as helper

--- app/helpers/activejob/web/application_helper.rb
+++ app/helpers/activejob/web/application_helper.rb
@@ -5,16 +5,16 @@ module Activejob
     module ApplicationHelper
       include Activejob::Web::SharedHelper
 
-      def signed_in_as
-        return 'Admin' if admin?
-
-        type = @activejob_web_current_user.parsed_class_name
-        if type == 'Common' && defined?(@job) && @job.present?
-          approver? ? 'Approver' : 'Executor'
-        else
-          type == 'Common' ? 'User' : type
-        end
-      end
+      # def signed_in_as
+      #   return 'Admin' if admin?
+      #
+      #   type = @activejob_web_current_user.parsed_class_name
+      #   if type == 'Common' && defined?(@job) && @job.present?
+      #     approver? ? 'Approver' : 'Executor'
+      #   else
+      #     type == 'Common' ? 'User' : type
+      #   end
+      # end
     end
   end
 end
