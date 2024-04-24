7f66c6e1ea5b781a65c1e047f2ac81d8b49f2fbc
Surender <surender@mallow-tech.com>
2024-04-24 17:11:18 +0530

    Refactor: Improvements to Authentication Module

--- app/controllers/concerns/activejob/web/authentication.rb
+++ app/controllers/concerns/activejob/web/authentication.rb
@@ -7,60 +7,57 @@ module Activejob
 
       included do
         before_action :validate_current_user_helper
-        before_action :activejob_web_current_user
-        helper_method :activejob_web_current_user
-        helper_method :user_signed_in?
-        helper_method :admin?
+        before_action :set_activejob_web_current_user
+        helper_method :activejob_web_current_user, :user_signed_in?, :admin?
       end
 
-      # Need to check the model is converted or not
       def activejob_web_current_user
-        host_user = fetch_host_user
-        redirect_to login_path, alert: t('user.invalid') unless host_user
-
-        @activejob_web_current_user = convert_user_model
+        @activejob_web_current_user ||= convert_user_model(fetch_host_user)
       end
 
       def user_signed_in?
         activejob_web_current_user.present?
       end
 
-      # Try to use the method instead of Instance
-      def admin?
-        return false unless @activejob_web_current_user.present? && validate_admin_model(@activejob_web_current_user)
-
-        return true unless @activejob_web_current_user.respond_to?(:admin?)
-
-        @activejob_web_current_user.admin?
+      def admin?(user = activejob_web_current_user)
+        @admin ||= user_admin?(user)
       end
 
       private
 
-      def current_user_helper
-        Activejob::Web.current_user_method.presence || :activejob_web_current_user
-      end
-
-      def fetch_host_user
-        @activejob_web_current_user ||= helpers.send(current_user_helper)
-      end
-
-      def convert_user_model
-        host_user_id = @activejob_web_current_user.id
-        if admin?
-          Activejob::Web::Admin.find(host_user_id)
-        elsif Activejob::Web.is_common_model
-          Activejob::Web::Common.find(host_user_id)
-        elsif valid_model?(@activejob_web_current_user, 'approvers')
-          Activejob::Web::Approver.find(host_user_id)
-        elsif valid_model?(@activejob_web_current_user, 'executors')
-          Activejob::Web::Executor.find(host_user_id)
-        end
-      end
-
       def validate_current_user_helper
         return if helpers.respond_to?(current_user_helper)
 
-        redirect_to login_path, alert: t('current_user_helper.method_missing')
+        raise t('current_user_helper.method_missing')
+        # redirect_to login_path, alert: t('current_user_helper.method_missing')
+      end
+
+      def set_activejob_web_current_user
+        @activejob_web_current_user = convert_user_model(fetch_host_user)
+      end
+
+      def fetch_host_user
+        helpers.send(current_user_helper) || user_invalid_redirect
+      end
+
+      def convert_user_model(user)
+        if admin?(user)
+          Activejob::Web::Admin.find(user.id)
+        elsif Activejob::Web.is_common_model
+          Activejob::Web::Common.find(user.id)
+        elsif valid_model?(user, 'approvers')
+          Activejob::Web::Approver.find(user.id)
+        elsif valid_model?(user, 'executors')
+          Activejob::Web::Executor.find(user.id)
+        end
+      end
+
+      def user_admin?(user)
+        return false unless user.present? && validate_admin_model(user)
+
+        return true unless user.respond_to?(:admin?)
+
+        user.admin?
       end
 
       def validate_admin_model(user)
@@ -69,8 +66,17 @@ module Activejob
         user.class.name.to_s == Activejob::Web.admins_model
       end
 
+      def current_user_helper
+        Activejob::Web.current_user_method.presence || :activejob_web_current_user
+      end
+
       def valid_model?(user, model)
         user.class.name.to_s == Activejob::Web.public_send("#{model}_model")
+      end
+
+      def user_invalid_redirect
+        # redirect_to login_path, alert: t('user.invalid')
+        raise t('user.invalid')
       end
     end
   end
