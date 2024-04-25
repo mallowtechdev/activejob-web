c8b875109405f075ac253878bce1721035f0e190
Surender <surender@mallow-tech.com>
2024-04-25 17:40:53 +0530

    Dashboard Changes

--- app/helpers/activejob/web/jobs_helper.rb
+++ app/helpers/activejob/web/jobs_helper.rb
@@ -6,6 +6,30 @@ module Activejob
       def common_model?
         !admin? && Activejob::Web.is_common_model
       end
+
+      def job_count(user)
+        user.jobs.count
+      end
+
+      def job_execution_count(user)
+        if admin?
+          Activejob::Web::JobExecution.count
+        elsif executor?
+          user.job_executions.count
+        else
+          0
+        end
+      end
+
+      def job_approval_count(user)
+        if admin?
+          Activejob::Web::JobApprovalRequest.count
+        elsif approver?
+          user.job_approval_requests.count
+        else
+          0
+        end
+      end
     end
   end
 end
