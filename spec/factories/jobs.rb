dca48362667b12c0414ac3bbc74908e217ccdf65
Surender <surender@mallow-tech.com>
2024-07-17 16:12:26 +0530

    Updated Job Model Spec

--- spec/factories/jobs.rb
+++ spec/factories/jobs.rb
@@ -5,5 +5,21 @@ FactoryBot.define do
     title { 'Activejob' }
     description { 'Web Gem' }
     job_name { 'TestJob' }
+    input_arguments do
+      [{ name: 'sample name',
+         type: 'String',
+         required: true,
+         allowed_characters: %w[<Regexp> test] }]
+    end
+    queue { 'default' }
+    max_run_time { 60 }
+    minimum_approvals_required { 0 }
+    priority { 0 }
+  end
+
+  factory :job_two, class: Activejob::Web::Job do
+    title { 'Activejob 2' }
+    description { 'Web Gem 2' }
+    job_name { 'TestJob2' }
   end
 end
--- spec/models/activejob/web/job_spec.rb
+++ spec/models/activejob/web/job_spec.rb
@@ -4,18 +4,85 @@ require 'rails_helper'
 
 RSpec.describe Activejob::Web::Job, type: :model do
   let(:job) { FactoryBot.create(:job) }
+  let(:new_job) { FactoryBot.build(:job) }
 
-  it 'is valid with valid attributes' do
-    expect(job).to be_valid
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      expect(job).to be_valid
+    end
+
+    it 'is not valid without a title' do
+      job.title = nil
+      expect(job).to_not be_valid
+      expect(job.errors[:title]).to include("can't be blank")
+    end
+
+    it 'is not valid without a job_name' do
+      job.job_name = nil
+      expect(job).to_not be_valid
+      expect(job.errors[:job_name]).to include("can't be blank")
+    end
+
+    it 'is not valid without a description' do
+      job.description = nil
+      expect(job).to_not be_valid
+      expect(job.errors[:description]).to include("can't be blank")
+    end
+
+    it 'is not valid with duplicate file arguments' do
+      new_job.input_arguments << { 'name' => 'sample file', 'type' => 'File' }
+      new_job.input_arguments << { 'name' => 'another sample file', 'type' => 'File' }
+      expect(new_job).to_not be_valid
+      expect(new_job.errors[:base]).to include("Duplicate 'File' types found.")
+    end
+
+    it 'is not valid with incorrect allowed_characters format' do
+      new_job.input_arguments = [{ name: 'sample name',
+                                   type: 'String',
+                                   required: true,
+                                   allowed_characters: %w[<Regexp>] }]
+      expect(new_job).to_not be_valid
+      expect(new_job.errors[:base]).to include("Invalid 'allowed_characters' input. Format must be ['<regex>', 'regex command']")
+    end
+
+    it 'is not valid with insufficient approvers' do
+      job.minimum_approvals_required = 2
+      expect(job).to_not be_valid
+      expect(job.errors[:base]).to include("Minimum Approver required is 2. Please select 2 more approver(s).")
+    end
+
+    it 'is valid with 0 approvers' do
+      job.minimum_approvals_required = 0
+      expect(job).to be_valid
+    end
   end
 
-  it 'is not valid without a title' do
-    job.title = nil
-    expect(job).to_not be_valid
+  describe 'associations' do
+    it 'has many approvers' do
+      expect(job.approvers.count).to eq(0)
+    end
+
+    it 'has many executors' do
+      expect(job.executors.count).to eq(0)
+    end
+
+    it 'has many job_executions' do
+      expect(job).to respond_to(:job_executions)
+    end
+
+    it 'has one attached template_file' do
+      expect(job).to respond_to(:template_file)
+    end
   end
 
-  it 'is not valid without a job_name' do
-    job.job_name = nil
-    expect(job).to_not be_valid
+  describe 'callbacks' do
+    let(:job_two) { FactoryBot.build(:job_two) }
+    it 'sets default values for queue, max_run_time, minimum_approvals_required, and priority' do
+      puts "JobTYwe: #{job_two.inspect}"
+      expect(job_two.queue).to eq('default')
+      expect(job_two.max_run_time).to eq(60)
+      expect(job_two.minimum_approvals_required).to eq(0)
+      expect(job_two.priority).to eq(1)
+    end
   end
 end
