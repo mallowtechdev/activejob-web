f6c7f90cb0277927d9c1d5adf3b2455d1b7238e0
Surender <surender@mallow-tech.com>
2024-07-26 11:57:07 +0530

    Removed unwanted test file

--- spec/requests/activejob/web/job_executions_spec.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-RSpec.describe Activejob::Web::JobExecutionsController, type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
-end
